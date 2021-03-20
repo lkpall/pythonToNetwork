8d974baa3adf2ae3d188f15c7c9e6b3c46a68621
Matheus Pinheiro <mfp186@hotmail.com>
2021-03-20 11:36:34 -0300

    Struct

--- ex_006_interface.py
+++ ex_006_interface.py
@@ -1,4 +1,4 @@
-from ex_006 import ex_006_funcoes as func
+import ex_006_funcoes as func
 
 # exemplo de como fazer a importação de outro arquivo nesse e utilizar funções dele
 nota1 = float(input("Digite a primeira nota:"))
